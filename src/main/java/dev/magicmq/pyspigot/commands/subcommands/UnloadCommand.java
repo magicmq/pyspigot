3a5d7291b01b94a47303d80a0db66d2c5dcda152
magicmq <mquindt@yahoo.com>
2021-03-29 13:39:59 -0500

    Fix unload command

--- src/main/java/dev/magicmq/pyspigot/commands/subcommands/UnloadCommand.java
+++ src/main/java/dev/magicmq/pyspigot/commands/subcommands/UnloadCommand.java
@@ -19,14 +19,14 @@ public class UnloadCommand implements SubCommand {
     @Override
     public boolean onCommand(CommandSender sender, String[] args) {
         if (args.length > 0) {
-            if (!ScriptManager.get().isScript(args[0])) {
+            if (ScriptManager.get().isScript(args[0])) {
                 boolean success = ScriptManager.get().unloadScript(args[0]);
                 if (success)
-                    sender.sendMessage(ChatColor.GREEN + "Successfully unloaded script " + args[0] + ". Run it with /pyspigot run " + args[0]);
+                    sender.sendMessage(ChatColor.GREEN + "Successfully unloaded script " + args[0]);
                 else
                     sender.sendMessage(ChatColor.GREEN + "Could not unload script " + args[0] + ". Unloading was cancelled by a listener.");
             } else {
-                sender.sendMessage(ChatColor.RED + "There is already a script with the name " + args[0]);
+                sender.sendMessage(ChatColor.RED + "No loaded script found with the name " + args[0]);
             }
             return true;
         }
