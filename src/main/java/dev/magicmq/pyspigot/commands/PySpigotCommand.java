2ab190f430885e39536a9c2160602cd350fbd527
Michael <mquindt@yahoo.com>
2021-08-15 20:00:42 -0500

    Add command to list scripts

--- src/main/java/dev/magicmq/pyspigot/commands/PySpigotCommand.java
+++ src/main/java/dev/magicmq/pyspigot/commands/PySpigotCommand.java
@@ -29,6 +29,7 @@ public class PySpigotCommand implements TabExecutor {
         subCommands.add(new LoadCommand());
         subCommands.add(new UnloadCommand());
         subCommands.add(new LoadLibraryCommand());
+        subCommands.add(new ListScriptsCommand());
         subCommands.sort((o1, o2) -> {
             SubCommandMeta subCommandMeta1 = o1.getClass().getAnnotation(SubCommandMeta.class);
             SubCommandMeta subCommandMeta2 = o2.getClass().getAnnotation(SubCommandMeta.class);
