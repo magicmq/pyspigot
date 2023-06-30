69d05f9c5f220c29f0a903239b5794803a69336b
Michael <mquindt@yahoo.com>
2023-06-30 03:59:52 -0500

    Clean up ListenerType names

--- src/main/java/dev/magicmq/pyspigot/manager/protocol/ListenerType.java
+++ src/main/java/dev/magicmq/pyspigot/manager/protocol/ListenerType.java
@@ -18,8 +18,8 @@ package dev.magicmq.pyspigot.manager.protocol;
 
 public enum ListenerType {
 
-    SYNCHRONOUS,
-    ASYNCHRONOUS_LISTENER,
+    NORMAL,
+    ASYNCHRONOUS,
     ASYNCHRONOUS_TIMEOUT
 
 }
