7691d456bce502a0e7a06c2b14ae2ed81d8b9289
Michael <mquindt@yahoo.com>
2023-09-05 17:38:19 -0500

    Add Javadocs to RunResult

--- src/main/java/dev/magicmq/pyspigot/manager/script/RunResult.java
+++ src/main/java/dev/magicmq/pyspigot/manager/script/RunResult.java
@@ -2,12 +2,24 @@ package dev.magicmq.pyspigot.manager.script;
 
 public enum RunResult {
 
+    /**
+     * Returned if the script ran successfully.
+     */
     SUCCESS,
 
+    /**
+     * Returned if the script was not run because it was disabled as per its script options in script_options.yml
+     */
     FAIL_DISABLED,
 
+    /**
+     * Returned if the script was not run because it has missing dependencies.
+     */
     FAIL_DEPENDENCY,
 
+    /**
+     * Returned if the script was run but failed during runtime due to an error.
+     */
     FAIL_ERROR
 
 }
