460b1320554a7d54c604b12653f5aa797fd561a3
Michael <mquindt@yahoo.com>
2023-09-22 17:19:25 -0500

    Renamed fields in script options for clarity

--- src/main/java/dev/magicmq/pyspigot/manager/script/ScriptOptions.java
+++ src/main/java/dev/magicmq/pyspigot/manager/script/ScriptOptions.java
@@ -30,8 +30,8 @@ public class ScriptOptions {
 
     private final boolean enabled;
     private final List<String> depend;
-    private final boolean loggingEnabled;
-    private final Level loggingLevel;
+    private final boolean fileLoggingEnabled;
+    private final Level minLoggingLevel;
 
     /**
      * Initialize a new ScriptOptions using values from the provided ConfigurationSection. If this constructor is passed a null value for the config parameter, then the default script options will be used.
@@ -41,13 +41,13 @@ public class ScriptOptions {
         if (config != null) {
             this.enabled = config.getBoolean("enabled", true);
             this.depend = config.getStringList("depend");
-            this.loggingEnabled = config.getBoolean("logging-enabled", PluginConfig.doLogToFile());
-            this.loggingLevel = Level.parse(config.getString("logging-level", PluginConfig.getLogLevel()));
+            this.fileLoggingEnabled = config.getBoolean("file-logging-enabled", PluginConfig.doLogToFile());
+            this.minLoggingLevel = Level.parse(config.getString("min-logging-level", PluginConfig.getLogLevel()));
         } else {
             this.enabled = true;
             this.depend = new ArrayList<>();
-            this.loggingEnabled = PluginConfig.doLogToFile();
-            this.loggingLevel = Level.parse(PluginConfig.getLogLevel());
+            this.fileLoggingEnabled = PluginConfig.doLogToFile();
+            this.minLoggingLevel = Level.parse(PluginConfig.getLogLevel());
         }
     }
 
@@ -71,15 +71,15 @@ public class ScriptOptions {
      * Get if file logging is enabled for this script.
      * @return True if file logging is enabled, false if otherwise
      */
-    public boolean isLoggingEnabled() {
-        return loggingEnabled;
+    public boolean isFileLoggingEnabled() {
+        return fileLoggingEnabled;
     }
 
     /**
      * Get the minimum logging level for this script, represented as a {@link java.util.logging.Level}
      * @return The minimum logging level at which messages should be logged
      */
-    public Level getLoggingLevel() {
-        return loggingLevel;
+    public Level getMinLoggingLevel() {
+        return minLoggingLevel;
     }
 }
