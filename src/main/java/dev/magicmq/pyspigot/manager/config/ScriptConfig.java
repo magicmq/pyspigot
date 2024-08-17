53722e7fb3b9e544ed9e5933cca0bcb058a65ae3
Michael <mquindt@yahoo.com>
2024-08-17 14:34:21 -0500

    Remove setDefaults method, add setIfNotExists method

--- src/main/java/dev/magicmq/pyspigot/manager/config/ScriptConfig.java
+++ src/main/java/dev/magicmq/pyspigot/manager/config/ScriptConfig.java
@@ -32,7 +32,7 @@ public class ScriptConfig extends YamlConfiguration {
 
     private final File configFile;
 
-    private String defaults;
+    private final String defaults;
 
     /**
      *
@@ -61,11 +61,18 @@ public class ScriptConfig extends YamlConfiguration {
     }
 
     /**
-     * Set the defaults for this configuration.
-     * @param defaults A YAML-formatted string containing the desired default values for the configuration
+     * Sets the specified path to the given value only if the path is not already set in the config file. Any specified default values are ignored when checking if the path is set.
+     * @see org.bukkit.configuration.ConfigurationSection#set(String, Object)
+     * @param path Path of the object to set
+     * @param value Value to set the path to
+     * @return True if the path was set to the value (in other words the path was not previously set), false if the path was not set to the value (in other words the path was already previously set)
      */
-    public void setDefaults(String defaults) {
-        this.defaults = defaults;
+    public boolean setIfNotExists(String path, Object value) {
+        if (!isSet(path)) {
+            super.set(path, value);
+            return true;
+        }
+        return false;
     }
 
     /**
