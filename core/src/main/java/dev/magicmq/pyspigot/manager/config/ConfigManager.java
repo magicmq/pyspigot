db1d3851af9c30ead891735af4723ded6445588d
Michael Q. <mquindt@yahoo.com>
2025-01-21 22:04:10 -0600

    Implement keyword arguments for config manager

--- core/src/main/java/dev/magicmq/pyspigot/manager/config/ConfigManager.java
+++ core/src/main/java/dev/magicmq/pyspigot/manager/config/ConfigManager.java
@@ -17,6 +17,8 @@
 package dev.magicmq.pyspigot.manager.config;
 
 import dev.magicmq.pyspigot.PyCore;
+import dev.magicmq.pyspigot.util.ArgParser;
+import org.python.core.PyObject;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -49,21 +51,28 @@ public abstract class ConfigManager {
     protected abstract ScriptConfig loadConfigImpl(Path configFile, String defaults) throws IOException;
 
     /**
-     * Load a configuration file with the given path/name, relative to the {@code configs} folder. If the configuration file exists, it will load the existing file. If the configuration file does not exist, a new file will be created with the given path/name.
+     * Load a configuration file with the given path/name, relative to the {@code configs} folder, with the specified defaults. If the configuration file exists, it will load the existing file. If the configuration file does not exist, a new file will be created with the given path/name.
      * <p>
      * <b>Note:</b> This should be called from scripts only!
-     * @param filePath The path of the configuration file to load, can be either the file name alone or a path (containing subfolders)
+     * <p>
+     * Arguments:
+     * <ul>
+     * <li>{@code file_path} (Required): The path of the configuration file to load, can be either the file name alone or a path (containing subfolders)</li>
+     * <li>{@code defaults} (Optional): A YAML-formatted string containing the desired default values for the configuration. If not specified, then no defaults will be used</li>
+     * </ul>
      * @return A ScriptConfig representing the configuration file that was loaded
      * @throws IOException If there was an IOException when attempting to load the configuration
      */
-    public ScriptConfig loadConfig(String filePath) throws IOException {
-        return loadConfig(filePath, null);
+    public ScriptConfig loadConfig(PyObject[] args, String[] keywords) throws IOException {
+        ArgParser argParser = new ArgParser("loadConfig", args, keywords, new String[]{"file_path", "defaults"}, 1);
+        String filePath = argParser.getString(0);
+        String defaults = argParser.getString(1, null);
+
+        return loadConfig(filePath, defaults);
     }
 
     /**
-     * Load a configuration file with the given path/name, relative to the {@code configs} folder. If the configuration file exists, it will load the existing file. If the configuration file does not exist, a new file will be created with the given path/name.
-     * <p>
-     * <b>Note:</b> This should be called from scripts only!
+     * Load a configuration file with the given path/name, relative to the {@code configs} folder, with the specified defaults. If the configuration file exists, it will load the existing file. If the configuration file does not exist, a new file will be created with the given path/name.
      * @param filePath The path of the configuration file to load, can be either the file name alone or a path (containing subfolders)
      * @param defaults A YAML-formatted string containing the desired default values for the configuration
      * @return A ScriptConfig representing the configuration file that was loaded
@@ -78,6 +87,21 @@ public abstract class ConfigManager {
      * Check if a configuration file exists with the given path/name, relative to the {@code configs} folder.
      * <p>
      * <b>Note:</b> This should be called from scripts only!
+     * Arguments:
+     * <ul>
+     * <li>{@code file_path} (Required): The path of the configuration file to check, can be either the file name alone or a path (containing subfolders)</li>
+     * </ul>
+     * @return True if the file exists, false if it does not
+     */
+    public boolean doesConfigExist(PyObject[] args, String[] keywords) {
+        ArgParser argParser = new ArgParser("doesConfigExist", args, keywords, new String[]{"file_path"}, 1);
+        String filePath = argParser.getString(0);
+
+        return doesConfigExist(filePath);
+    }
+
+    /**
+     * Check if a configuration file exists with the given path/name, relative to the {@code configs} folder.
      * @param filePath The path of the configuration file to check, can be either the file name alone or a path (containing subfolders)
      * @return True if the file exists, false if it does not
      */
@@ -88,9 +112,27 @@ public abstract class ConfigManager {
 
     /**
      * Creates a config file with the given path if it did not previously exist.
-     * @param filePath The file path for the config file to create
-     * @throws IOException If there was an IOException when attempting to create the config file
+     * <p>
+     * <b>Note:</b> This should be called from scripts only!
+     * Arguments:
+     * <ul>
+     * <li>{@code file_path} (Required): The file path for the config file to create, relative to the {@code configs} folder. Can be either the file name alone or a path (containing subfolders)</li>
+     * </ul>
      * @return The {@link Path} to the config file
+     * @throws IOException If there was an IOException when attempting to create the config file
+     */
+    public Path createConfigFileIfNotExists(PyObject[] args, String[] keywords) throws IOException {
+        ArgParser argParser = new ArgParser("createConfigFileIfNotExists", args, keywords, new String[]{"file_path"}, 1);
+        String filePath = argParser.getString(0);
+
+        return createConfigIfNotExists(filePath);
+    }
+
+    /**
+     * Creates a config file with the given path if it did not previously exist.
+     * @param filePath The file path for the config file to create, relative to the {@code configs} folder. Can be either the file name alone or a path (containing subfolders)
+     * @return The {@link Path} to the config file
+     * @throws IOException If there was an IOException when attempting to create the config file
      */
     public Path createConfigIfNotExists(String filePath) throws IOException {
         Path configFile = configFolder.resolve(Paths.get(filePath));
@@ -107,6 +149,22 @@ public abstract class ConfigManager {
      * Delete a configuration file with the given path/name.
      * <p>
      * <b>Note:</b> This should be called from scripts only!
+     * Arguments:
+     * <ul>
+     * <li>{@code file_path} (Required): The path of the configuration file to delete, relative to the {@code configs} folder. Can be either the file name alone or a path (containing subfolders)</li>
+     * </ul>
+     * @return True if the file was deleted, false if the file could not be deleted because it does not exist
+     * @throws IOException If there was an IOException when attempting to delete the file
+     */
+    public boolean deleteConfig(PyObject[] args, String[] keywords) throws IOException {
+        ArgParser argParser = new ArgParser("deleteConfig", args, keywords, new String[]{"file_path"}, 1);
+        String filePath = argParser.getString(0);
+
+        return deleteConfig(filePath);
+    }
+
+    /**
+     * Delete a configuration file with the given path/name
      * @param filePath The path of the configuration file to delete, relative to the {@code configs} folder. Can be either the file name alone or a path (containing subfolders)
      * @return True if the file was deleted, false if the file could not be deleted because it does not exist
      * @throws IOException If there was an IOException when attempting to delete the file
